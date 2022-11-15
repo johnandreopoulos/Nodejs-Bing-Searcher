d4b5eb2a8c770ba257d1ffd77df233eaec883efe
JohnAndreop <39243722+JohnAndreop@users.noreply.github.com>
2022-11-15 14:33:20 +0200

    Returning results

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { Search } = require('./searcher');
 // Fill in the 'query' with whatever you want to search for.
 Search('query').then((data) => {
     if (data.error) return console.log(data);
-    else console.log(data);
+    else return console.log(data);
 });
