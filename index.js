290f40585a56969fba672b65bee395e6c406cca4
JohnAndreop <39243722+JohnAndreop@users.noreply.github.com>
2022-11-15 14:24:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Search } = require('./searcher');
 
 // Fill in the 'query' with whatever you want to search for.
-Search('123123123123123123q45wd4sdq').then((data) => {
+Search('query').then((data) => {
     if (data.error) return console.log(data);
     else console.log(data);
 });
