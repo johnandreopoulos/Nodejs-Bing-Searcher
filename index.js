cfef664e2eb5a9903f62677918def2ebd97f4929
JohnAndreop <39243722+JohnAndreop@users.noreply.github.com>
2022-12-22 22:16:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const { Search } = require('./searcher');
+const { search } = require('./searcher');
+
+async function runSearch(query) {
+  try {
+    const data = await search(query);
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // Fill in the 'query' with whatever you want to search for.
-Search('query').then((data) => {
-    if (data.error) return console.log(data);
-    else return console.log(data);
-});
+runSearch('google');
--- searcher.js
+++ searcher.js
@@ -1,46 +1,49 @@
-const cheerio = require('cheerio');
-const { fetch } = require('undici');
-
-let data = [];
-const Search = async (query) => {
+async function search(query) {
     const headers = {
         'accept-language': 'en-US,en;q=0.9',
         'set-cookie': 'path=/; domain=.bing.com; HttpOnly; Secure; SameSite=None',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
+    };
 
-    let response = await fetch(`https://www.bing.com/search?q=${query}`, {
-        method: 'GET',
-        headers
-    });
+    try {
+        const response = await fetch(`https://www.bing.com/search?q=${query}`, {
+            method: 'GET',
+            headers
+        });
 
-    if (response.status === 404) error(404);
-    if (response.status === 500) error(500);
-    if (response.status === 503) error(503);
-    if (response.status === 504) error(504);
+        const $ = cheerio.load(await response.text());
+        const data = [];
 
-    let $ = cheerio.load(await response.text());
-    await $('.b_algo').each((i, el) => {
-        let title = $(el).find('h2').text() ?? null;
-        let link = $(el).find('a').attr('href') ?? null;
-        let content = $(el).find('p').text() ?? null;
+        await $('.b_algo').each((i, el) => {
+            const title = $(el).find('h2').text() || null;
+            const link = $(el).find('a').attr('href') || null;
+            const content = $(el).find('p').text() || null;
 
-        if (title && link && content) {
-            data.push({ title, link, content });
+            if (title && link && content) {
+                data.push({ title, link, content });
+            }
+        });
+
+        if (data.length < 1) {
+            throw new Error('No Results');
         }
-    });
 
-    if (data.length < 1) error('No Results');
-
-    return data;
+        return data;
+    } catch (error) {
+        if (error.message === 'No Results') {
+            return { error: true, message: 'No Results' };
+        } else if (error.status === 404) {
+            return { error: true, message: '404 Not Found' };
+        } else if (error.status === 500) {
+            return { error: true, message: '500 Internal Server Error' };
+        } else if (error.status === 503) {
+            return { error: true, message: '503 Service Unavailable' };
+        } else if (error.status === 504) {
+            return { error: true, message: '504 Gateway Timeout' };
+        } else {
+            return { error: true, message: error.message };
+        }
+    }
 }
 
-function error(x) {
-    if (x === 404) return data.push({ error: true, message: '404 Not Found' });
-    if (x === 500) return data.push({ error: true, message: '500 Internal Server Error' });
-    if (x === 503) return data.push({ error: true, message: '503 Service Unavailable' });
-    if (x === 504) return data.push({ error: true, message: '504 Gateway Timeout' });
-    if (x === 'No Results') return data.push({ error: true, message: 'No Results' });
-}
-
-module.exports = { Search };
+module.exports = { search };
